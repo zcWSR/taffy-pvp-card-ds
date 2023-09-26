44ffa440d91034c572cee1889a11447e22a29e6c
zcWSR <zhaocong@zcwsr.com>
2023-09-26 15:47:49 +0800

    fix: update script

--- pushUpdate.sh
+++ pushUpdate.sh
@@ -3,11 +3,17 @@
 set -eo pipefail
 
 cd $GITHUB_WORKSPACE/taffy-pvp-card-ds
+
 git config --global user.name 'github-actions'
 git config --global user.email 'github-actions@github.com'
 git add .
+
 if [ -n "`git status -s`" ]; then
     git commit -m "update at `date +"%Y-%m-%d %H:%M:%S"`"
     git push
-    curl -X POST http://qqbot.zcwsr.com/japari/message -d "type=genshinUpdate"
+
+    COMMIT_FILES=`echo "[$(git log -1 --pretty=format:'' --name-only | sed -e '$!s/\(.*\)$/"\1",/' -e '$s/\(.*\)$/"\1"/' | tr -d "\n")]"`
+    curl -X POST http://qqbot.zcwsr.com/japari/message \
+    -H "Content-Type: application/json" \
+    -d "{\"type\":\"genshinUpdate\",\"data\":$COMMIT_FILES}"
 fi
